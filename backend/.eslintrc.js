20dfe8dd1f6e068ce01d6ddb7b856ac7516fd6bc
Konrad Stasiuk <konrad.stasiuk@venturelabs.team>
2022-03-12 17:24:10 +0100

    Added .env settings

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': false }],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from '~/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // @ts-ignore
+  await app.listen(process.env.APP_PORT);
+  // eslint-disable-next-line no-console
+  console.log('App is listening on port:', process.env.APP_PORT);
 }
 bootstrap();
