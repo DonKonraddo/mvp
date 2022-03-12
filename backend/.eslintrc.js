cbd5f078b77395c736badbb2f2ce8126d568f48a
Konrad Stasiuk <konrad.stasiuk@venturelabs.team>
2022-03-12 15:57:36 +0100

    Set code style for backend

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'simple-import-sort'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -17,8 +17,17 @@ module.exports = {
   ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'require-await': 'error',
+
+    'no-console': ['error', { allow: ['warn', 'error'] }],
   },
 };
